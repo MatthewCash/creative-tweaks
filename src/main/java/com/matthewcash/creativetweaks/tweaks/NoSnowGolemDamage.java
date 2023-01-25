6d9293e5c070fa2415322a51d964f2bb2528cd18
Matthew_Cash <matthew@matthew-cash.com>
2023-01-24 21:13:59 -0800

    Only prevent named snow golems from taking damage

--- src/main/java/com/matthewcash/creativetweaks/tweaks/NoSnowGolemDamage.java
+++ src/main/java/com/matthewcash/creativetweaks/tweaks/NoSnowGolemDamage.java
@@ -2,6 +2,7 @@ package com.matthewcash.creativetweaks.tweaks;
 
 import org.bukkit.event.EventPriority;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -9,8 +10,10 @@ import org.bukkit.event.entity.EntityDamageEvent;
 public class NoSnowGolemDamage implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onEntityDamage(EntityDamageEvent event) {
-        if (event.getEntity().getType() == EntityType.SNOWMAN) {
+        LivingEntity entity = (LivingEntity) event.getEntity();
+        if (entity.getType() == EntityType.SNOWMAN && !entity.getName().equals("Snow Golem")) {
             event.setCancelled(true);
+            entity.setHealth(4);
         }
     }
 }
