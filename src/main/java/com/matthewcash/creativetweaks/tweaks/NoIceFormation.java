b3d50b4b84317f827949dfad0b1f7acf9cf3267a
Matthew_Cash <matthew@matthew-cash.com>
2023-01-29 17:03:12 -0800

    Fix formatting

--- src/main/java/com/matthewcash/creativetweaks/tweaks/NoIceFormation.java
+++ src/main/java/com/matthewcash/creativetweaks/tweaks/NoIceFormation.java
@@ -4,10 +4,11 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFormEvent;
 
 public class NoIceFormation implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onIceFormation(org.bukkit.event.block.BlockFormEvent event) {
+    public void onIceFormation(BlockFormEvent event) {
         if (event.getNewState().getType() == Material.ICE) {
             event.setCancelled(true);
         }
